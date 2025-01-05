b06a62e1ab6a18b10028a91fd9019e786d1d0895
Catherine <catherineamf@gmail.com>
2025-01-05 22:39:11 +0200

    first commit

--- main.tf
+++ main.tf
@@ -1,1 +1,86 @@
+provider "aws" {
+  region = var.region
+}
+
+resource "aws_vpc" "main_vpc" {
+  cidr_block       = var.cidr
+  instance_tenancy = "default"
+
+  tags = {
+    Name = "${var.environment}-vpc"
+  }
+}
+
+resource "aws_subnet" "public_subnet" {
+  vpc_id                  = aws_vpc.main_vpc.id
+  cidr_block              = var.publicCIDR
+  availability_zone       = var.availability_zone
+  map_public_ip_on_launch = true
+
+  tags = {
+    Name = "${var.environment}-public-subnet"
+  }
+}
+
+resource "aws_internet_gateway" "igw" {
+  vpc_id = aws_vpc.main_vpc.id
+
+  tags = {
+    Name = "${var.environment}-igw"
+  }
+}
+
+resource "aws_route_table" "public_rt" {
+  vpc_id = aws_vpc.main_vpc.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.igw.id
+  }
+
+  tags = {
+    Name = "${var.environment}-public-rt"
+  }
+}
+
+resource "aws_route_table_association" "public_assoc" {
+  subnet_id      = aws_subnet.public_subnet.id
+  route_table_id = aws_route_table.public_rt.id
+}
+
+resource "aws_security_group" "ec2_sg" {
+  vpc_id = aws_vpc.main_vpc.id
+
+  dynamic "ingress" {
+    for_each = var.allowed_ports
+    content {
+      from_port   = ingress.value
+      to_port     = ingress.value
+      protocol    = "tcp"
+      cidr_blocks = ["0.0.0.0/0"]
+    }
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "${var.environment}-sg"
+  }
+}
+
+resource "aws_instance" "ec2_instance" {
+  ami                    = var.instance_AMI
+  instance_type          = var.instance_type
+  subnet_id              = aws_subnet.public_subnet.id
+  vpc_security_group_ids = [aws_security_group.ec2_sg.id]
+
+  tags = {
+    Name = "${var.environment}-ec2-instance"
+  }
+}
 
--- outputs.tf
+++ outputs.tf
@@ -1,27 +1,23 @@
-output "ec2_public_ip" {
-  put your code here
-}
-
 output "ec2_ami" {
-  put your code here
-}
-
-output "ec2_type" {
-  put your code here
-}
-
-output "public_vpc_id" {
-  put your code here
-}
-
-output "ec2_subnet_id" {
-  put your code here
+  value = aws_instance.ec2_instance.ami
 }
 
 output "public_subnet_AZ" {
-  put your code here
+  value = aws_subnet.public_subnet.availability_zone
 }
 
-output "ec2_region" {
-  put your code here
+output "ec2_type" {
+  value = aws_instance.ec2_instance.instance_type
+}
+
+output "ec2_public_ip" {
+  value = aws_instance.ec2_instance.public_ip
+}
+
+output "public_vpc_id" {
+  value = aws_vpc.main_vpc.id
+}
+
+output "ec2_subnet_id" {
+  value = aws_subnet.public_subnet.id
 }
